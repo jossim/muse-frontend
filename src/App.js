edd8ed3c2a7bc302ef916d46b6d341bd154b2d63
Joseph Simmons <josephdsimmons@gmail.com>
2021-09-21 15:55:35 -0500

    Finished most of AllArtists component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import axios from "axios";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 import AllArtists from "./components/AllArtists/AllArtists";
 import ArtistDetail from "./components/AllArtists/ArtistDetail/ArtistDetail";
@@ -22,15 +22,43 @@ class App extends Component {
     this.setState({ artists: response.data.artists });
   }
 
-  addArtist = async () => {};
+  addArtist = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post(this.apiUrl, {
+      name: event.target.name.value
+    });
+
+    event.target.name.value = '';
+
+    const tempArtists = this.state.artists;
+    tempArtists.push(response.data.artist);
+
+    this.setState({ artists: tempArtists });
+  };
+
   addSong = async () => {};
 
   render() {
     return (
       <div className="App">
         <h1>Muse App for Music!</h1>
+
+        <nav>
+          <Link to='/'>Home</Link> | <Link to='/artists'>All artists</Link>
+        </nav>
+
         <Switch>
-          <Route path="/artists" component={AllArtists} />
+          <Route 
+            path="/artists" 
+            exact 
+            component={() => <AllArtists 
+              artists={this.state.artists}
+              addArtist={this.addArtist}
+            />} 
+          />
+
+          <Route path="/artists/:id" component={ArtistDetail} />
         </Switch>
       </div>
     );
--- src/components/AllArtists/AllArtists.js
+++ src/components/AllArtists/AllArtists.js
@@ -1,10 +1,26 @@
 import React from "react";
-import './AllArtists.css';
+import "./AllArtists.css";
+import { Link } from "react-router-dom";
 
 export default function AllArtists(props) {
+  const artists = props.artists.map((artist) => {
+    return (
+      <li key={artist.id}>
+        <Link to={`/artists/${artist.id}`}>{artist.name}</Link>
+      </li>
+    );
+  });
+
   return (
     <div className="AllArtists">
       <h2>All Artists</h2>
+
+      <form onSubmit={props.addArtist}>
+        <input type="text" name="name" />
+        <input type="submit" value="Add Artist" />
+      </form>
+
+      <ul>{artists}</ul>
     </div>
   );
 }
